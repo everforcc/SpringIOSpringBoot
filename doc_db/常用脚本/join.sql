d68690a563dc8caeb114583621ca694f7f939c83
everforcc <718497737@qq.com>
2022-07-20 11:23:32 +0800

    2022-07-10 1. join sql示例 2. validation实现类源码示例
    Signed-off-by: everforcc <718497737@qq.com>

--- doc_db/常用脚本/join.sql
+++ doc_db/常用脚本/join.sql
@@ -2,3 +2,9 @@
 
 SELECT e.*,a.* FROM cc_website e LEFT JOIN cc_websitedata a ON e.`id` = a.`parentid`
 WHERE e.`id` = 1;
+
+-- left join 三个表
+SELECT e.`id` 'eid',a.`id` 'aid',c.`id` 'cid' FROM cc_website e
+LEFT JOIN cc_websitedata a ON e.`id` = a.`parentid`
+LEFT JOIN cc_websitetag c ON a.`id` = c.`webrootid`
+WHERE e.`id` = '1' ;
