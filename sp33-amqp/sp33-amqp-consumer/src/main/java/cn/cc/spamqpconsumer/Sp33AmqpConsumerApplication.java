09b17ba93b525dd9a60d1c998a20bdb9daa0366a
c.c <718497737@qq.com>
2024-12-10 17:15:31 +0800

    调整 sp33-amqp 目录结构

---
+++ sp33-amqp/sp33-amqp-consumer/src/main/java/cn/cc/spamqpconsumer/Sp33AmqpConsumerApplication.java
@@ -0,0 +1,13 @@
+package cn.cc.spamqpconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sp33AmqpConsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sp33AmqpConsumerApplication.class, args);
+    }
+
+}
---
+++ sp33-amqp/sp34-amqp-publisher/src/main/java/cn/cc/spamqppublisher/Sp34AmqpPublisherApplication.java
@@ -0,0 +1,13 @@
+package cn.cc.spamqppublisher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sp34AmqpPublisherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sp34AmqpPublisherApplication.class, args);
+    }
+
+}
