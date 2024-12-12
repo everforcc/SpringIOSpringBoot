4c36b71f87ced54017e549fdbf854851eecd073d
c.c <718497737@qq.com>
2024-12-12 19:49:57 +0800

    调整 sp31-mqtt 目录结构

---
+++ sp31-mqtt/sp31-mqtt-provider/src/main/java/cn/cc/Sp31MqttProviderApplication.java
@@ -0,0 +1,13 @@
+package cn.cc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sp31MqttProviderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sp31MqttProviderApplication.class, args);
+    }
+
+}
---
+++ sp31-mqtt/sp31-mqtt-provider/src/test/java/cn/cc/Sp31MqttProviderApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.cc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sp31MqttProviderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
