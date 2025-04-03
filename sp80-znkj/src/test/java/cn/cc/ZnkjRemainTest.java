b4d3f9ef2ff72d654c94b97f8b719cd0de0fe124
c.c <718497737@qq.com>
2025-04-03 17:57:39 +0800

    znkj日期测试

---
+++ sp80-znkj/src/test/java/cn/cc/ZnkjRemainTest.java
@@ -0,0 +1,42 @@
+package cn.cc;
+
+import org.junit.Test;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+
+public class ZnkjRemainTest {
+
+    @Test
+    public void remainDay() {
+        // 获取当前日期
+        LocalDate today = LocalDate.now();
+        // 设置要比较的日期，例如：2023年3月15日
+        LocalDate specificDate = LocalDate.of(2026, 2, 1);
+        // 计算两个日期之间的天数差
+        long daysBetween = ChronoUnit.DAYS.between(today, specificDate);
+
+        // 输出结果
+        System.out.println("从今天到指定日期还有 " + daysBetween + " 天");
+        if (daysBetween < 0) {
+            System.out.println("注意：指定日期已过。");
+        }
+
+        int hour = LocalDateTime.now().minusHours(7).getHour();
+
+        System.out.println(hour);
+
+        String str = "1";
+        System.out.println(str.charAt(0));
+
+        LocalDateTime now = LocalDateTime.now();
+
+        LocalDateTime endTime = LocalDateTime.of(now.getYear(), now.getMonth(), now.getDayOfMonth(), now.getHour(), 0, 0);
+        LocalDateTime startTime = endTime.minusDays(1);
+
+        System.out.println(startTime);
+        System.out.println(endTime);
+    }
+
+}
