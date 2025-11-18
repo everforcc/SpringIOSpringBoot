fae35c732fe155524c271c6d61de8b5cee4723cc
c.c <718497737@qq.com>
2025-11-18 15:48:04 +0800

    增加exam模块

---
+++ cc-exam/cc-exam-algo/src/test/java/cn/cc/algo/AlgoTest.java
@@ -0,0 +1,14 @@
+package cn.cc.algo;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+
+@Slf4j
+public class AlgoTest {
+
+    @Test
+    public void test() {
+        log.info("测试");
+    }
+
+}
---
+++ cc-exam/cc-exam-structure/src/test/java/cn/cc/struct/StructTest.java
@@ -0,0 +1,14 @@
+package cn.cc.struct;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+
+@Slf4j
+public class StructTest {
+
+    @Test
+    public void test() {
+        log.info("测试");
+    }
+
+}
